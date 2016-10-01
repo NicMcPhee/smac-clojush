993a52576ffe6284a74bf458daa634fc47e3c661
mcphee@morris.umn.edu <mcphee@morris.umn.edu>
2016-10-01 14:01:21 -0400

    Add Python "go-between" script
    This adds a slightly modified version of the Python "go-between"
    script that connects SMAC and Clojush. We cleaned up a few bits
    of historical cruft and changed the Clojush problem into a command
    line argument so the script won't have to be changed when you go
    from one problem to another.
    
    There is still quite a lot of refactoring that can be done here.
    The handling of command line arguments is pretty atrocious, for
    example, and should definitely be improved.

---
+++ run-clojush.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+import sys
+import os
+import signal
+import string
+from subprocess import Popen, PIPE
+import subprocess
+import re
+
+# For black box function optimization, we can ignore the first 5 arguments.
+# The remaining arguments specify parameters using this format: -name value
+
+popsize = 0
+parent_selection = '":lexicase"'
+alt_prob = 0.2
+uni_mut_prob = 0.2
+uni_close_mut = 0.1
+alt_mut = 0.5
+alt_rate = 0.01
+alignment_dev = 10
+uni_mut_rate = 0.01
+
+problem = sys.argv[1]
+
+wallclock_limit = float(sys.argv[4])
+
+for i in range(len(sys.argv)-1):
+    if (sys.argv[i] == '-popsize'):
+        popsize = int(sys.argv[i+1])
+    elif (sys.argv[i] == '-selection'):
+        parent_selection = sys.argv[i+1]
+    elif(sys.argv[i] == '-alt_prob'):
+        alt_prob = float(sys.argv[i+1])
+    elif(sys.argv[i] == '-uni_mut_prob'):
+        uni_mut_prob = float(sys.argv[i+1])
+    elif(sys.argv[i] == '-uni_close_mut'):
+        uni_close_mut = float(sys.argv[i+1])
+    elif(sys.argv[i] == '-alt_mut'):
+        alt_mut = float(sys.argv[i+1])
+    elif(sys.argv[i] == '-alt_rate'):
+        alt_rate = float(sys.argv[i+1])
+    elif(sys.argv[i] == '-alignment_dev'):
+        alignment_dev = int(sys.argv[i+1])
+    elif(sys.argv[i] == '-uni_mut_rate'):
+        uni_mut_rate = float(sys.argv[i+1])
+
+gen_prob_total = alt_prob + uni_mut_prob + uni_close_mut + alt_mut
+# If the probabilities all add up to 0 (i.e., they are all 0), then
+# we return a "failed" score and exit.
+if gen_prob_total == 0:
+    score = 1
+    print "Result for SMAC: SUCCESS, 0, 0, %d, 0" % score
+    sys.exit()
+else:
+    alt_prob = alt_prob / gen_prob_total
+    uni_mut_prob = uni_mut_prob / gen_prob_total
+    uni_close_mut = uni_close_mut / gen_prob_total
+    alt_mut = alt_mut / gen_prob_total
+
+# The max evals used in @thelmuth's dissertation work.
+max_evaluations = 1000 * 300
+num_generations = int(max_evaluations/popsize)
+
+arg_dict = {}
+arg_dict[":max-generations"] = num_generations
+arg_dict[":population-size"] = popsize
+arg_dict[":parent-selection"] = parent_selection
+arg_dict[":genetic-operator-probabilities"] = '\"{:alternation %f :uniform-mutation %f :uniform-close-mutation %f [:alternation :uniform-mutation] %f}\"' % (alt_prob, uni_mut_prob, uni_close_mut, alt_mut)
+arg_dict[":alternation-rate"] = alt_rate
+arg_dict[":alignment-deviation"] = alignment_dev
+arg_dict[":uniform-mutation-rate"] = uni_mut_rate
+
+uberjar = "clojush-2.0.73-SNAPSHOT-standalone.jar"
+command = ["./runsolver", "-W", str(wallclock_limit), "java", "-jar", uberjar, problem]
+for key in arg_dict:
+    command = command + [key, str(arg_dict[key])]
+
+sys.stderr.write(str(command) + "\n")
+
+io = Popen(' '.join(command), stdout=PIPE, shell=True)
+(stdout_, stderr_) = io.communicate()
+
+score = 1
+lines = stdout_.splitlines()
+for line in lines:
+    m = re.search("SUCCESS", line)
+    if m:
+        score = 0
+
+print "Result for SMAC: SUCCESS, 0, 0, %d, 0" % score
